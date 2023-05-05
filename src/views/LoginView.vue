2d8437cb7059456e81d0002707eedcac2a54f396
AlanCienega <craulalan115@gmail.com>
2023-05-04 21:50:05 -0600

    login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>Login</div>
+  <div>
+    <pre>
+      {{ user }}
+    </pre>
+    <form @submit.prevent="login">
+      <input v-model="form.email" type="email" placeholder="email" />
+      <br />
+      <input type="password" name="password" />
+      <br />
+      <button>Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    user: {},
+    form: {
+      email: "",
+      password: "",
+    },
+  }),
+  methods: {
+    login() {
+      console.log("login");
+    },
+  },
+};
 </script>
